21bb68acf5e18660e63d3af4d8c639c9bc5196b0
MysticalMike60t <caden.finkelstein@cttech.org>
2024-03-23 13:57:42 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,7 +6,10 @@ from urllib.parse import urlparse
 
 mode = 0o777
 
-print("Enter the URL:")
+scrapeOutputDirectory = "./output/scrapes"
+defaultPageFileName = "index.html"
+
+print("Enter the URL with connection type (http:// or https://):")
 url = input().strip()
 
 if not (url.startswith('http://') or url.startswith('https://')):
@@ -17,12 +20,12 @@ response = requests.get(url)
 
 def create_site_folder_from_scrape(url):
     domain = urlparse(url).netloc
-    folder_path = os.path.join("./output/scrapes", domain)
+    folder_path = os.path.join(scrapeOutputDirectory, domain)
     
     if not os.path.exists(folder_path):
         os.makedirs(folder_path, mode)
         
-    with open(os.path.join(folder_path, "index.html"), "w", encoding="utf-8") as f:
+    with open(os.path.join(folder_path, defaultPageFileName), "w", encoding="utf-8") as f:
         soup = bs(response.content, 'html.parser')
         prettyHTML = soup.prettify()
         f.write(prettyHTML)
